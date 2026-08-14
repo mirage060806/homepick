ee1688f8eac81b0090fa718a98556a66f54394c1
m <mirage060806@naver.com>
2026-08-14 13:10:00 +0900

    0814

--- src/main/java/com/render/homepick/service/QnaService.java
+++ src/main/java/com/render/homepick/service/QnaService.java
@@ -1,5 +1,25 @@
 package com.render.homepick.service;
 
-public class QnaService {
-    
+import com.render.homepick.dto.QnaDto;
+import com.render.homepick.repository.QnaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QnaService{
+
+    private final QnaRepository qnaRepository;
+
+    public QnaService(QnaRepository qnaRepository){
+        this.qnaRepository = qnaRepository;
+    }
+
+    public List<QnaDto> getAllQnaList(){
+        return qnaRepository.findAll();
+    }
+
+    public void createQna(QnaDto qna){
+        qnaRepository.save(qna);
+    }
 }
